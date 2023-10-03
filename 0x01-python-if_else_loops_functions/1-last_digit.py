b39b6a0bf06b6c8e0bda782f37335f91fced76c7
ehabw57 <maestor.ehab123@gmail.com>
2023-10-04 00:09:43 +0300

    TypeError huh?

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print(f"Last digit of {number:d} is", end="")
+print(f"Last digit of {number} is", end="")
 if number < 0:
     Lastdig = -number % 10
     Lastdig *= -1
 else:
     Lastdig = number % 10
-print(f" {Lastdig:d}", end="")
+print(f" {Lastdig}", end="")
 if Lastdig > 5:
     print(f" and is greater than 5", end="")
 if Lastdig == 0:
