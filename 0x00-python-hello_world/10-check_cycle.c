eaa10077ef033b6ca3bb7bf1a8be7ac7366540b0
ehabw57 <maestor.ehab123@gmail.com>
2023-10-04 14:09:55 +0300

    Effcinye in the bag

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,23 +1,24 @@
 #include "lists.h"
+
 /**
-*check_cycle - checks cycles in a linked list
-*@list: the list to check
-*Return: 0 if cycle is found 1 if not
-*/
+ * check_cycle - checks cycles in a linked list
+ * @list: the list to check
+ * Return: 0 if cycle is found, 1 if not
+ */
 int check_cycle(listint_t *list)
 {
-	listint_t *tor = NULL, *hare = NULL, *tmp = NULL;
-	tor = list;
-	tmp = list->next;
-	hare = tmp->next;
+	listint_t *slow, *fast;
 
-	while(tmp->next != NULL)
+	slow = fast = list;
+
+	while (fast != NULL && fast->next != NULL)
 	{
-		tmp = hare->next;
-		hare = tmp->next;
-		tor = tor->next;
-		if (hare == tor)
-		return (1);
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+			return (1);
 	}
+
 	return (0);
 }
