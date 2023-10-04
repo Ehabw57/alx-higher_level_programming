ccc315b5befbc44bbce4e5fb6e100611b6021605
ehabw57 <maestor.ehab123@gmail.com>
2023-10-04 23:03:11 +0300

    Got my test cases in the bag

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -2,10 +2,22 @@
 #include <stdlib.h>
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *currnt = *head, *forward = (*head)->next;
+	listint_t *currnt = *head, *forward = *head;
+
+	if (*head == NULL || forward->n > number)
+	{
+		forward = malloc(sizeof(listint_t));
+		if (forward == NULL)
+				return (NULL);
+		forward->n = number;
+		forward->next = *head;
+		*head = forward;
+		return (*head);
+	}
 
 	while (currnt != NULL)
 	{
+		forward = forward->next;
 		if (forward == NULL || forward->n > number)
 		{
 			forward = malloc(sizeof(listint_t));
@@ -16,8 +28,7 @@ listint_t *insert_node(listint_t **head, int number)
 			currnt->next = forward;
 			return (*head);
 		}
-		forward = forward->next;
-		currnt  = currnt->next;
+		currnt = currnt->next;
 	}
 
 	return (NULL);
