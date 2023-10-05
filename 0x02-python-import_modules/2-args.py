c49ca4a11b7c48324294d943724904f4ebbfc607
ehabw57 <maestor.ehab123@gmail.com>
2023-10-05 23:34:23 +0300

    Uploded task 2 file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,4 +3,4 @@ if __name__ == "__main__":
     from sys import argv
     print("{} arguments:".format(len(argv) - 1))
     for i in range(1, len(argv)):
-        print("{} : {}".format(i, argv[i]))
+        print("{}: {}".format(i, argv[i]))
