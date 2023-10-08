aaadf486ce0ea4f612eafc44ae98509d7b4cfdf4
ehabw57 <maestor.ehab123@gmail.com>
2023-10-08 15:08:46 +0300

    tuple yes

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
-        for lst in matrix:
-            if len(lst) > 0:
-                print(f"{lst[0]}", end="")
-            for element in lst[1:]:
-                print(" ", end="")
-                print("{:d}".format(element), end="")
-            print("")
+    for lst in matrix:
+        if len(lst) > 0:
+            print(f"{lst[0]}", end="")
+        for element in lst[1:]:
+            print(" {:d}".format(element), end="")
+        print("")
