fe63ee49d752e836c3b5584de9ab6290abae9c34
Ehabw57 <maestro.ehab123@gmail.com>
2023-10-10 01:42:15 -0700

    got my effcint

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,38 +2,32 @@
 #include <stdio.h>
 int is_palindrome(listint_t **head)
 {
-    listint_t *move = *head, *new;
+    listint_t *move = NULL;
     int *nums = NULL;
-    int lenth = 0, min = 0;
+    int lenth = 0, i = 0;
 
     /*clac the lenth of the list*/
-    while (move != NULL)
-    {
+    for (move = (*head); move != NULL; lenth++)
         move = move->next;
-        lenth++;
-    }
-    /*checks if list is only 1 node*/
-    if (lenth <= 1 || ((*head)->n == (*head)->next->n && lenth == 2))
+
+    /*checks if list is only 1 node or less*/
+    if (lenth <= 1 || head == NULL)
         return 1;
-    /*move ptr to the half of the list*/
-    for (move = (*head); min <= lenth / 2; min++)
-        move = move->next;
+        
+    i = lenth / 2;    
+    nums = malloc(sizeof(int) * i);
+    
+    /*move ptr to the half of the list while copying it reversly*/
+    for (move = (*head); i > 0; i--)
+        {  
+            nums[i - 1] = move->n;
+            move = move->next;
+        }
 
-    nums = malloc(sizeof(int) * (min + 1));
-    new = move;
-    /*store half of the list*/
-    while (min >= 0 && move != NULL)
+    /*compare both list and nums*/
+    for (i = 0; nums[i] == move->n; i++)
     {
-        nums[min] = move->n;
-        move = move->next;
-        min--;
-    }
-    move = new;
-    /*loop on the list*/
-    for (min = 0; nums[min] == move->n; min++)
-    {
-
-        if (min >= lenth / 2)
+        if (i >= (lenth / 2) - 1)
         {
             free(nums);
             return (1);
