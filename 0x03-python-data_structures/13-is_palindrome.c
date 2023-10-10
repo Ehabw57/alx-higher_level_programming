aa293e8eefebcc20c675985aae1f5381f955fd80
ehabw57 <maestor.ehab123@gmail.com>
2023-10-10 14:04:35 +0300

    is palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,39 +1,43 @@
 #include "lists.h"
-#include <stdio.h>
+/**
+ * is_palindrome - checks whether a linked list is palindorme or not
+ * @head: pointer to the head of the list
+ * Return: 1 if list is palindrome, 0 if not
+*/
 int is_palindrome(listint_t **head)
 {
-    listint_t *move = NULL;
-    int *nums = NULL;
-    int lenth = 0, i = 0;
+	listint_t *move = NULL;
+	int *nums = NULL;
+	int lenth = 0, i = 0;
 
-    /*clac the lenth of the list*/
-    for (move = (*head); move != NULL; lenth++)
-        move = move->next;
+	/*clac the lenth of the list*/
+	for (move = (*head); move != NULL; lenth++)
+		move = move->next;
 
-    /*checks if list is only 1 node or less*/
-    if (lenth <= 1 || head == NULL)
-        return 1;
-        
-    i = lenth / 2;    
-    nums = malloc(sizeof(int) * i);
-    
-    /*move ptr to the half of the list while copying it reversly*/
-    for (move = (*head); i > 0; i--)
-        {  
-            nums[i - 1] = move->n;
-            move = move->next;
-        }
+	/*checks if list is only 1 node or less*/
+	if (lenth <= 1 || *head == NULL)
+		return (1);
 
-    /*compare both list and nums*/
-    for (i = 0; nums[i] == move->n; i++)
-    {
-        if (i >= (lenth / 2) - 1)
-        {
-            free(nums);
-            return (1);
-        }
-        move = move->next;
-    }
-    free(nums);
-    return (0);
+	i = lenth / 2;
+	nums = malloc(sizeof(int) * i);
+
+	/*move ptr to the half of the list while copying it reversly*/
+	for (move = (*head); i > 0; i--)
+	{
+		nums[i - 1] = move->n;
+		move = move->next;
+	}
+
+	/*compare both list and nums*/
+	for (i = 0; nums[i] == move->n; i++)
+	{
+		if (i >= (lenth / 2) - 1)
+		{
+			free(nums);
+			return (1);
+		}
+		move = move->next;
+	}
+	free(nums);
+	return (0);
 }
