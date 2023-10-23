15221d418e4d578ca5b03733b0beedbe9cc982fe
Ehabw57 <maestro.ehab123@gmail.com>
2023-10-24 00:33:52 +0300

    reuploded task 4 file

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -10,7 +10,7 @@ def list_division(my_list_1, my_list_2, list_length):
             continue
         except IndexError:
             result = 0
-            return my_result
+            print('out of range')
         except ZeroDivisionError:
             result = 0
             print('division by 0')
