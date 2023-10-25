22c8fce6be71753ae44fc2208e377af17c3560cc
Ehabw57 <maestro.ehab123@gmail.com>
2023-10-26 01:57:30 +0300

    Fixed a bug in the grater than sign

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -73,7 +73,11 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if value[0] > 0 or value[1] > 0:
+        if (
+            value[0] < 0
+            or value[1] < 0
+            or type(value[1]) is not type(value[0]) is not int
+        ):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.position = value
 
