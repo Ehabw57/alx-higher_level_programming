0305875a114cc3d893881b300bd037fcccebbe6f
Ehabw57 <maestro.ehab123@gmail.com>
2023-11-01 09:35:07 -0700

    I dont like formal commits

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,14 +8,23 @@ class Rectangle:
     Just A Rectangle class
     """
     def __init__(self, width=0, height=0):
+        """
+        initalize a rextangle
+        
+        Args:
+            width (int): width of RQ
+            height (int): height of Rq
+        """
         self.__width = width
         self.__height = height
     @property
     def width(self):
+        """return the width of Rq"""
         return self.__width
 
-    @property.setter
+    @width.setter
     def width(self, value):
+        """set the width of RQ"""
         if type(value) is not int:
             raise TypeError('width must be an integer')
         if value < 0:
@@ -24,10 +33,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """return the hieght of RQ"""
         return self.__height
     
-    @property.setter
+    @height.setter
     def height(self, value):
+        """set the height of Rq"""
         if type(value) is not int:
             raise TypeError('height must be an integer')
         if value < 0:
