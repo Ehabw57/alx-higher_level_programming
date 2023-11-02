a51cbc6e1e60d587f4ffe5fb30e87d7365588528
Ehabw57 <maestro.ehab123@gmail.com>
2023-11-02 23:42:06 +0200

    mod 0-add_int

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -15,6 +15,6 @@ def add_integer(a, b=98):
     """
     if type(a) not in [int, float] or a is None:
         raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
+    if type(b) not in [int, float] or b is None:
         raise TypeError("b must be an integer")
     return int(a) + int(b)
