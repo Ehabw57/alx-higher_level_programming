b7e7153a99f1db47f07a35a10bd3206b4df7d3a9
Ehabw57 <maestro.ehab123@gmail.com>
2023-11-02 08:23:12 +0200

    Reqtangle yeah

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""
+moudle of class reqtangle
+"""
+
+
+class Rectangle:
+    """
+    Just A Rectangle class
+    """
+
+    def __init__(self, width=0, height=0):
+        """
+        initalize a rextangle
+
+        Args:
+            width (int): width of RQ
+            height (int): height of Rq
+        """
+        self.__height = height
+        self.__width = width
+
+    @property
+    def width(self):
+        """return the width of Rq"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set the width of RQ"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """return the hieght of RQ"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set the height of Rq"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return the area of Reqtangle"""
+        return self.height * self.width
+
+    def perimeter(self):
+        """Return the perimeter of Reqtangle"""
+        if self.height == 0 or self.width == 0:
+            return 0
+        return 2 * (self.height + self.width)
+
+    def __str__(self):
+        """Print # based on the Reqtangle"""
+        if self.height == 0 or self.width == 0:
+            return ""
+        s = ("#" * self.width) + "\n"
+        string = s * (self.height - 1)
+        string += "#" * self.width
+        return string
+
+    def __repr__(self):
+        """Returns the diminstions of Rq"""
+        return f"Rectangle({self.width}, {self.height})"
