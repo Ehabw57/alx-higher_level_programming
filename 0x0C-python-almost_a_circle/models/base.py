707fb7e5312498936c25867f8518e3487312093e
Ehab Hegazy <136044546+Ehabw57@users.noreply.github.com>
2023-11-14 00:16:32 +0200

    Added some docsring to baee.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-"""some docs goes here"""
+"""This module defines the Base class."""
 class Base:
+    """The Base class represents a base object."""
     __nb_objects = 0
     def __init__(self, id=None):
-        """some docs goes here"""
+        """Initialize a new Base instance.
+
+        Args:
+            id (int, optional): The ID of the object. Defaults to None.
+        """
         if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
         else:
             self.id = id
-
-    
