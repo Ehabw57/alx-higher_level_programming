b3bcad1cca629749776967d5cb5e5b90e4e6bb7c
Ehabw57 <maestro.ehab123@gmail.com>
2024-02-11 16:58:38 +0200

    Uploded some tasks

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,2 @@
+-- Create data base names hbtn_0c_0 if it's not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- Drop database names hbtn_0c_0 if it's not exist
+DROP DATABASE IF EXISTS hbtn_0c_0;
