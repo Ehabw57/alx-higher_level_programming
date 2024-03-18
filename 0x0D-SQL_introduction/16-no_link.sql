cfb60bbc6e510cc4b44c3180e3150757e4f4aaa5
Ehabw57 <maestor.ehab123@gmail.com>
2024-03-18 13:20:40 +0200

    order

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- Display all rows score, name where name is not null
-SELECT score, name FROM second_table WHERE `name` IS NOT NULL;
+SELECT score, name FROM second_table WHERE `name` IS NOT NULL ORDER BY `score` DESC;
