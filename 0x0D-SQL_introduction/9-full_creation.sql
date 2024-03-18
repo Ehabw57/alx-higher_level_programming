066ad85015302976d786b0ced639d652e4b650fd
Ehabw57 <maestor.ehab123@gmail.com>
2024-03-18 12:08:51 +0200

    yeah new fie

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,5 @@
+-- Create a table second_table (id INT, name VARCHAR(256), score INT)
+-- Inser values (1, John, 10), (2, Alex, 3), (3, Bob, 14), (4, George,8)
+CREATE TABLE second_table(id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score)
+VALUES(1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
