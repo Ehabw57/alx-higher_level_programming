c8d1a93d6d7dfac348cc545263a32531a400f81c
Ehabw57 <maestor.ehab123@gmail.com>
2024-04-07 15:30:00 +0200

    Uplode more table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- Create a new databaes hbtn_0d_usa with states table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(256) NOT NULL)
