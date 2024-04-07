57c230ee8eded6fcae1f828f8a9b451fcd750e8e
Ehabw57 <maestor.ehab123@gmail.com>
2024-04-07 17:58:00 +0200

    Uplolded the new table oh

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- Create new table cities (id int , state_id int, name vharchar(256))
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+id INT AUTO_INCREMENT PRIMARY KEY,
+state_id INT NOT NULL, name VARCHAR(256) NOT NULL,
+FOREIGN KEY(state_id) REFERENCES states(id));
+
