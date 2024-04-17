7e8c1b1e4dcdbd68d7cfb9f1d6138d95e3cc2762
Ehabw57 <maestor.ehab123@gmail.com>
2024-04-18 00:36:44 +0200

    fixed some indentation

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """A script that quring a table using sqlalchemy lib"""
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sys import argv
 
-args = argv[1:]
-engine = create_engine('mysql://{}:{}@localhost/{}'
-                       .format(argv[1], argv[2], argv[3]))
 if __name__ == '__main__':
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from sys import argv
+
+    engine = create_engine('mysql://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
