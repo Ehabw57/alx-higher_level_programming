c010558b4a8b26ddeeb578c5548620372a3dd979
Ehabw57 <maestor.ehab123@gmail.com>
2024-04-18 22:35:20 +0200

    yeah new state

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Adds a new row to the db using sqlalchemy"""
+if __name__ == '__main__':
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from sys import argv
+
+    usrname, password, dbname = argv[1:]
+    engine = create_engine('mysql://{}:{}@localhost:3306/{}'
+                           .format(usrname, password, dbname), echo=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    louis = State(name='Louisiana')
+    session.add(louis)
+    louis = session.query(State).filter(State.name == louis.name).first()
+    print(louis.id)
