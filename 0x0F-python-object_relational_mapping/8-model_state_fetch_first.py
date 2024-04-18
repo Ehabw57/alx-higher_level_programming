b32e5a6284d5a46a25d37119c05aa60bd889965d
Ehabw57 <maestor.ehab123@gmail.com>
2024-04-18 21:12:09 +0200

    fixed task 8 to not be ex when imported

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 """List the first row in the db hbtn_0e_6_usa using sql alchemy"""
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sys import argv
+if __name__ == '__main__':
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from sys import argv
 
-usrname, passwod, dbname = argv[1:]
-engine = create_engine('mysql://{}:{}@localhost:3306/{}'
-                       .format(usrname, passwod, dbname))
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
+    usrname, passwod, dbname = argv[1:]
+    engine = create_engine('mysql://{}:{}@localhost:3306/{}'
+                           .format(usrname, passwod, dbname))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-first = session.query(State).first()
-if first:
-    print('{}: {}'.format(first.id, first.name))
-else:
-    print('Nothing')
+    first = session.query(State).first()
+    if first:
+        print('{}: {}'.format(first.id, first.name))
+    else:
+        print('Nothing')
