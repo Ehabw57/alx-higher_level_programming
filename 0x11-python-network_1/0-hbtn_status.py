06b74a619d139f751a28e30bff005412d6a1f6c3
Ehabw57 <maestor.ehab123@gmail.com>
2024-08-16 21:11:31 +0300

    i hate it

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """Python script that fetches https://alx-intranet.hbtn.io/status"""
 
-from urllib.request import urlopen
-with urlopen("https://alx-intranet.hbtn.io/status") as respond:
-    body = respond.read()
-    print("Body response:")
-    print("    - type: {}".format(type(body)))
-    print("    - content: {}".format(body))
-    print("    - utf8 content: {}".format(body.decode("utf-8")))
+
+if __name__ == "__main__":
+    from urllib.request import urlopen
+
+    with urlopen("https://alx-intranet.hbtn.io/status") as respond:
+        body = respond.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
