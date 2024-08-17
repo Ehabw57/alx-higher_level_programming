60370cdd06180f19d31fdf920a2f51e8630d35ea
Ehabw57 <maestor.ehab123@gmail.com>
2024-08-17 14:23:13 +0300

    i always win

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Write a Python script that takes in a letter and sends a
+POST request to http://0.0.0.0:5000/search_user with
+the letter as a parameter.
+"""
+
+
+from requests import post
+from sys import argv
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    data = {"q": ""}
+    if len(argv) > 1:
+        data = {"q": argv[1]}
+    respond = post(url, data=data)
+    content = respond.json()
+    if not isinstance(content, dict):
+        print("Not a valid JSON")
+    else:
+        if len(content) < 2:
+            print("No result")
+        else:
+            print("[{}] {}".format(content["id"], content["name"]))
+
