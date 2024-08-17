c5b18a40c783494ddbdb7cdb09947c735a11c4e3
Ehabw57 <maestor.ehab123@gmail.com>
2024-08-17 14:31:35 +0300

    changed prmission

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -14,12 +14,11 @@ if __name__ == "__main__":
     if len(argv) > 1:
         data = {"q": argv[1]}
     respond = post(url, data=data)
-    content = respond.json()
-    if not isinstance(content, dict):
-        print("Not a valid JSON")
-    else:
+    try:
+        content = respond.json()
         if len(content) < 2:
             print("No result")
         else:
-            print("[{}] {}".format(content["id"], content["name"]))
-
+            print("[{}] {}".format(content.get("id"), content.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
