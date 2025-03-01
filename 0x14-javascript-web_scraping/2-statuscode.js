cf0c211da3a0eafd8c31552b6e67bcfcc271cf58
Ehabw57 <maestro.ehab123@gmail.com>
2025-03-01 23:46:23 +0200

    oh yeah

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 const URL = process.argv[2];
 async function main () {
-  const response = await fetch(URL);
+  const request = new Request(URL);
+  const response = await fetch(request);
   console.log(response.status);
 }
 main();
